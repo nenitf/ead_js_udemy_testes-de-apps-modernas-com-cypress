cbd1265a706ad83053cbaf0f717f6bff2f34c247
Felipe Silva <felipe.dacs@gmail.com>
2021-04-20 20:28:54 -0300

    :tv: add aula 19

--- cypress/integration/sync.spec.js
+++ cypress/integration/sync.spec.js
@@ -15,11 +15,36 @@ describe('Espera...', () => {
     cy.get('#novoCampo').type('funciona')
   })
 
-  it.only('Deve fazer retries', () => {
+  it('Deve fazer retries', () => {
     cy.get('#novoCampo').should('not.exist')
     cy.get('#buttonDelay').click()
     cy.get('#novoCampo')
       //.should('not.exist')  // retorna null pois não existe
       .should('exist')      // erro
+  })
+
+  it.only('Uso do find', () => {
+    cy.get('#buttonList').click()
+    cy.get('#lista li')
+      .find('span')
+      .should('contain', 'Item 1')
+
+    // retries ocorrem com assertivas e não com comandos (find)
+    // portanto caso tenha delay o find não será rodado novamente
+    // e a assertiva fica presa ao escopo anterior
+    //
+    // no exemplo:
+    //
+    // - Item 2 demorou para aparecer
+    // - Falhou na assertiva
+    // - O comando cy.get('#lista li') foi refeito (ignora find)
+    // - Escopo da segunda li foi perdido
+    //
+    // cy.get('#lista li')
+      // .find('span')
+      // .should('contain', 'Item 2')
+
+    cy.get('#lista li span')
+      .should('contain', 'Item 1')
   })
 })
