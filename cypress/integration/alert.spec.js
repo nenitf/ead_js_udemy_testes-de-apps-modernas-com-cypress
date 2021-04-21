ee19fa62f8612dabe58d36d75b9e5d95a4308377
Felipe Silva <felipe.dacs@gmail.com>
2021-04-21 13:59:54 -0300

    :tv: add aula 33

--- cypress/integration/alert.spec.js
+++ cypress/integration/alert.spec.js
@@ -49,7 +49,7 @@ describe('Work with alert', () => {
     cy.get('#confirm').click()
   })
 
-  it.only('Prompt', () => {
+  it('Prompt', () => {
     cy.window().then(win => {
       cy.stub(win, 'prompt').returns('42')
     })
@@ -62,5 +62,28 @@ describe('Work with alert', () => {
     })
 
     cy.get('#prompt').click()
+  })
+
+  it.only('Validando mensagens', () => {
+    const stub = cy.stub().as('alerta')
+    cy.on('window:alert', stub)
+    cy.get('#formCadastrar').click().then(() => {
+      expect(stub.getCall(0)).to.be.calledWith('Nome eh obrigatorio')
+    })
+
+    cy.get('#formNome').type('Wagner')
+    cy.get('#formCadastrar').click().then(() => {
+      expect(stub.getCall(1)).to.be.calledWith('Sobrenome eh obrigatorio')
+    })
+
+    cy.get('[data-cy=dataSobrenome]').type('Aquino')
+    cy.get('#formCadastrar').click().then(() => {
+      expect(stub.getCall(2)).to.be.calledWith('Sexo eh obrigatorio')
+    })
+
+    cy.get('#formSexoMasc').click()
+    cy.get('#formCadastrar').click()
+
+    cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado!')
   })
 })
