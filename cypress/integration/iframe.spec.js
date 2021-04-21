db8677518924fcdd032e8e21804ef6d1cf8c14b5
Felipe Silva <felipe.dacs@gmail.com>
2021-04-21 14:32:00 -0300

    :tv: add aula 35

--- cypress/integration/iframe.spec.js
+++ cypress/integration/iframe.spec.js
@@ -1,5 +1,5 @@
 describe('Work with iframe', () => {
-  it.only('deve preencher campo de texto', () => {
+  it('deve preencher campo de texto', () => {
     cy.visit('http://wcaquino.me/cypress/componentes.html')
     cy.get('#frame1').then(iframe => {
       const body = iframe.contents().find('body')
@@ -11,7 +11,20 @@ describe('Work with iframe', () => {
         expect(msg).eq('Alert Simples')
       })
 
-      cy.wrap(body).find('#otherButton').click()
+      // cy.wrap(body).find('#otherButton').click() // alert do iframe foge do escopo do cypress
     })
+  })
+
+  it('deve testar iframe diretamente', () => {
+    cy.visit('http://wcaquino.me/cypress/frame.html')
+    cy.get('#tfield')
+      .type('funciona?')
+      .should('have.value', 'funciona?')
+
+    cy.on('window:alert', msg => {
+      expect(msg).eq('Click OK!')
+    })
+
+    cy.get('#otherButton').click()
   })
 })
