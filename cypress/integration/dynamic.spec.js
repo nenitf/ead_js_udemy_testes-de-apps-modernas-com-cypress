37bfd140830739243520427a00cb005138a3677d
Felipe Silva <felipe.dacs@gmail.com>
2021-04-21 17:31:45 -0300

    :tv: add aula 42

--- cypress/integration/dynamic.spec.js
+++ cypress/integration/dynamic.spec.js
@@ -6,7 +6,7 @@ describe('Dynamic tests', () => {
   const foods = ['carne', 'frango', 'pizza', 'vegetariano']
 
   foods.forEach(food => {
-    it.only(`Deve selecionar ${food}`, () => {
+    it(`Deve selecionar ${food}`, () => {
       cy.get('#formNome').type('Usuario qualquer')
       cy.get('#formSobrenome').type('Sobrenome qualquer')
       cy.get('[name=formSexo][value=F]').click()
@@ -17,5 +17,26 @@ describe('Dynamic tests', () => {
       cy.get('#formCadastrar').click()
       cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado!')
     })
+  })
+
+  it.only('deve selecionar todos usando each', () => {
+    cy.get('#formNome').type('Usuario qualquer')
+    cy.get('#formSobrenome').type('Sobrenome qualquer')
+    cy.get('[name=formSexo][value=F]').click()
+
+    // todos incluindo veg
+    // cy.get(`[name=formComidaFavorita]`).click({multiple: true})
+    // cy.clickAlert('#formCadastrar', 'Tem certeza que voce eh vegetariano?')
+
+    // filtra veg
+    cy.get(`[name=formComidaFavorita]`).each((el) => {
+      if(el.val() !== 'vegetariano') {
+        // el.click() // jquery.click perde a rastreabilidade (painel esquerdo) do cypress
+        cy.wrap(el).click()
+      }
+    })
+
+    cy.get('#formEscolaridade').select('Doutorado')
+    cy.get('#formEsportes').select('Corrida')
   })
 })
